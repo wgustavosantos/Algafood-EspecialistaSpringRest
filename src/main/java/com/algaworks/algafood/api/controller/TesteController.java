9930ded94a0c39c8da63d4043ca3136aae243e14
Wenderson Santos <guto15santos@gmail.com>
2022-06-27 11:25:19 -0300

    Query Methods

--- src/main/java/com/algaworks/algafood/api/controller/TesteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/TesteController.java
@@ -1,28 +1,42 @@
-//package com.algaworks.algafood.api.controller;
-//
-//import java.util.List;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import com.algaworks.algafood.domain.model.Cozinha;
-//import com.algaworks.algafood.domain.repository.CozinhaRepository;
-//
-//@RestController
-//@RequestMapping(value = "/teste")
-//public class TesteController {
-//	
-//	@Autowired
-//	private CozinhaRepository cozinhaRepository;
-//
-//	@GetMapping(value = "/cozinhas/por-nome")
-//	public List<Cozinha> cozinhasPorNome(@RequestParam("nomee")   String nomee) {
-//		
-//		return cozinhaRepository.consultarPorNome(nomee);
-//		
-//	}
-//
-//}
+package com.algaworks.algafood.api.controller;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
+
+@RestController
+@RequestMapping(value = "/teste")
+public class TesteController {
+
+	@Autowired
+	private RestauranteRepository restauranteRepository;
+
+	@GetMapping(value = "/restaurante/por-nome")
+	public List<Restaurante> restaurantePorNome(String nome) {
+
+		return restauranteRepository.findByNomeContaining(nome);
+
+	}
+	
+	@GetMapping(value = "/restaurante/por-nome-e-taxa")
+	public List<Restaurante> restaurantePorNomeAndTaxa(String nome, BigDecimal taxaInicial, BigDecimal taxaFinal) {
+
+		return restauranteRepository.findByNomeContainingAndTaxaFreteBetween(nome, taxaInicial, taxaFinal);
+
+	}
+	
+	@GetMapping(value = "/restaurante/por-nome-e-cozinha")
+	public List<Restaurante> restaurantePorNomeAndCozinhaId(String nome, Long cozinhaId) {
+
+		return restauranteRepository.findByNomeContainingAndCozinhaId(nome, cozinhaId);
+
+	}
+
+}
--- src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -1,4 +1,6 @@
-package com.algaworks.algafood.domain.repository;
+ package com.algaworks.algafood.domain.repository;
+
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,5 +9,7 @@ import com.algaworks.algafood.domain.model.Cozinha;
 
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
+	
+	List<Cozinha> findByNome(String nome);
 
 }
